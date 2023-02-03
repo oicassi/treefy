417f4e8789f101fe1d5202383a10d6cf9c447520
Cassiano Kruchelski <casst@protonmail.com>
2023-02-02 22:53:32 -0300

    add fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { useState } from 'react';
-import { storage } from '@/utils';
 
 export default function Home() {
-  const [storedData, setStoredData] = useState('');
-
-  const handleStoreData = () => {
-    storage.setItem('testezinho', 'pipipi popopo');
-  };
-
-  const handleGetData = () => {
-    const s = storage.getItem('testezinho');
-    setStoredData(s);
-  };
-
   return (
     <>
       <Head>
@@ -24,14 +11,8 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>
-        <Image priority src='/images/profile.jpeg' height={144} width={144} alt='profile pic' />
-
-        <button onClick={handleStoreData}>Store</button>
-        <br />
-        <button onClick={handleGetData}>GetData</button>
-        <br />
-        <br />
-        <p>{typeof storedData !== 'string' ? JSON.stringify(storedData) : storedData}</p>
+        <h1>Teste</h1>
+        <p>Testezinho</p>
       </main>
     </>
   );
