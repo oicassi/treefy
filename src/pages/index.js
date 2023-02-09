735449ef5f1e6529ebd2599a3a599d3138c96fba
Cassiano Kruchelski <casst@protonmail.com>
2023-02-09 10:01:37 -0300

    add main content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 import Layout from '@/components/Layout';
 import Button from '@/components/Button';
+import { useMediaQuery } from '@/utils/hooks';
 import styles from '@/styles/pages/index.module.scss';
 import utilsStyles from '@/styles/base/utils.module.scss';
-import { helpers } from '@/utils';
-const { If } = helpers;
+
 export default function Home() {
-  const handleClick = () => {
-    console.log('clicked');
-  };
+  const isMobile = useMediaQuery('mobile');
   return (
     <Layout>
       <Head>
@@ -20,7 +20,7 @@ export default function Home() {
       <div className={styles.container}>
         <section className={utilsStyles.container}>
           <div className={styles.textImage}>
-            <p>
+            <p className={styles.textImage__text}>
               treefy is a simple web application that allows you to view{' '}
               <span className={styles.highlight}>your most recently played artists on Spotify</span> in the form of a
               <span className={styles.highlight}>small field with some trees</span>, where the size of the trees and
@@ -31,14 +31,31 @@ export default function Home() {
               </span>
               . <strong>That simple :)</strong>
             </p>
+            <Image
+              priority
+              src='/images/about.png'
+              className={styles.textImage__image}
+              height={isMobile ? 113 : 160}
+              width={isMobile ? 294 : 415}
+              alt='Diagram explaining that the recent top artists data come from spotify to be used in treefy'
+            />
           </div>
         </section>
         <section className={utilsStyles.container}>
-          <If condition={false} renderIf={<p>opa</p>} renderElse={<p>ih</p>} />
-
-          <If condition={true}>
-            <p>aiaiai</p>
-          </If>
+          <h2 className={styles.sectionTitle}>and what about my data?</h2>
+          <p className={styles.text}>
+            your data will be secure! treefy will only read the info about your top artists and nothing more. The login
+            process is completely handled by Spotify so the application doesn’t have any access to your credentials. If
+            you want to know more about the login and authorization process,{' '}
+            <a
+              href='https://developer.spotify.com/documentation/general/guides/authorization/code-flow/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              you can check here the documentation from Spotify
+            </a>
+            . You can also check the <strong>treefy’s</strong> <Link href='/privacy'>privacy section</Link>.
+          </p>
         </section>
       </div>
     </Layout>
